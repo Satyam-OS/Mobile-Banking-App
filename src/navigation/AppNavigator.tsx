204a256b4e7fcf5f0f2904224fd63e140e24f519
hritik2807 <hritik.pandit@theoptimum.net>
2026-05-04 14:55:16 +0530

    Updated AppNavigator

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,5 +1,5 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
+// update
 import AdminDashboard from "../screens/AdminDashboard";
 import BalanceSecurity from "../screens/BalanceSecurity";
 import BankTransfer from "../screens/BankTransfer";
